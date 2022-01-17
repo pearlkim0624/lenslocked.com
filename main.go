d015dc74f9b977ea08082e707cdef05c440512c5
Pearl Kim <pearlkim0624@gmail.com>
2022-01-17 10:33:30 -0600

    Created a controller for our static pages, and we also updated the View type to implement the http.Handler interface

--- main.go
+++ main.go
@@ -4,35 +4,17 @@ import (
 	"net/http"
 
 	"lenslocked.com/controllers"
-	"lenslocked.com/views"
 
 	"github.com/gorilla/mux"
 )
 
-// temporarily use global variable
-var (
-	homeView    *views.View
-	contactView *views.View
-)
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	http.ListenAndServe(":3000", r)
