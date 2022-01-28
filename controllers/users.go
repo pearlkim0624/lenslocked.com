ae253c9f9229c300db274605b128e9f37d3ad8c6
Pearl Kim <pearlkim0624@gmail.com>
2022-01-27 22:01:16 -0600

    Implemeneted better error handling for Users.Create action

--- controllers/users.go
+++ controllers/users.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"lenslocked.com/models"
@@ -29,14 +30,7 @@ type Users struct {
 // New is used to render the form where a user can create a new user account.
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	d := views.Data{
-		Alert: &views.Alert{
-			Level:   views.AlertLvError,
-			Message: "something went wrong",
-		},
-		Yield: "Hello!!!",
-	}
-	if err := u.NewView.Render(w, d); err != nil {
+	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
 }
@@ -51,9 +45,16 @@ type SignupForm struct {
 // This is used to create a new user account.
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	var vd views.Data
 	var form SignupForm
 	if err := parseForm(r, &form); err != nil {
-		panic(err)
+		log.Println(err)
+		vd.Alert = &views.Alert{
+			Level:   views.AlertLvError,
+			Message: views.AlertMsgGeneric,
+		}
+		u.NewView.Render(w, vd)
+		return
 	}
 
 	user := models.User{
@@ -62,13 +63,17 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		Password: form.Password,
 	}
 	if err := u.us.Create(&user); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		vd.Alert = &views.Alert{
+			Level:   views.AlertLvError,
+			Message: err.Error(),
+		}
+		u.NewView.Render(w, vd)
 		return
 	}
 
 	err := u.signIn(w, &user)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Redirect(w, r, "/login", http.StatusFound)
 		return
 	}
 	http.Redirect(w, r, "/cookietest", http.StatusFound)
--- views/data.go
+++ views/data.go
@@ -5,6 +5,10 @@ const (
 	AlertLvWarning = "warning"
 	AlertLvInfo    = "info"
 	AlertLvSuccess = "success"
+
+	// AlertMsgGeneric is displayed when any random error
+	// is encountered by our backend.
+	AlertMsgGeneric = "Something went wrong. Please try again, and contact us if the problem persists."
 )
 
 // Alert is used to render Bootstrap Alert messages in templates
