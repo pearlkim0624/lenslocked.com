09eab2d0e190b43f097a5e19f17b3bfdad21bf0f
Pearl Kim <pearlkim0624@gmail.com>
2022-01-31 14:21:17 -0600

    Created the gallery model

---
+++ models/galleries.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// Gallery is our image container resources that visitors view
+type Gallery struct {
+	// type Model struct {
+	//	ID        uint `gorm:"primary_key"`
+	//	CreatedAt time.Time
+	//	UpdatedAt time.Time
+	//	DeletedAt *time.Time `sql:"index"`
+	// }
+	gorm.Model
+	UserID uint   `gorm:"not null;index"`
+	Title  string `gorm:"not null"`
+}
