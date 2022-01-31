f124a901307941f4bb269fbb3ef0a396ff69f4c8
Pearl Kim <pearlkim0624@gmail.com>
2022-01-31 17:37:20 -0600

    Created the galleries controller

---
+++ controllers/galleries.go
@@ -0,0 +1,141 @@
+package controllers
+
+import (
+	"lenslocked.com/models"
+	"lenslocked.com/views"
+)
+
+func NewGalleries(gs models.GalleryService) *Galleries {
+	return &Galleries{
+		New: views.NewView("bootstrap", "galleries/new"),
+		gs:  gs,
+	}
+}
+
+type Galleries struct {
+	New *views.View
+	gs  models.GalleryService
+}
+
+/*
+// New is used to render the form where a user can create a new user account.
+// GET /signup
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	u.NewView.Render(w, nil)
+}
+
+type SignupForm struct {
+	Name     string `schema:"name"`
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
+// Create is used to process the signup form when a user submits it.
+// This is used to create a new user account.
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	var vd views.Data
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
+		log.Println(err)
+		vd.SetAlert(err)
+		u.NewView.Render(w, vd)
+		return
+	}
+
+	user := models.User{
+		Name:     form.Name,
+		Email:    form.Email,
+		Password: form.Password,
+	}
+	if err := u.us.Create(&user); err != nil {
+		vd.SetAlert(err)
+		u.NewView.Render(w, vd)
+		return
+	}
+
+	err := u.signIn(w, &user)
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
+}
+
+type LoginForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
+// Login is used to verify the provided email address and
+// password and then log the user in if they are correct.
+// POST /login
+func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
+	vd := views.Data{}
+	var form LoginForm
+	if err := parseForm(r, &form); err != nil {
+		log.Println(err)
+		vd.SetAlert(err)
+		u.LoginView.Render(w, vd)
+		return
+	}
+
+	// Authenticating users
+	user, err := u.us.Authenticate(form.Email, form.Password)
+	if err != nil {
+		switch err {
+		case models.ErrNotFound:
+			vd.AlertError("Invalid email address")
+		default:
+			vd.SetAlert(err)
+		}
+		u.LoginView.Render(w, vd)
+		return
+	}
+	err = u.signIn(w, user)
+	if err != nil {
+		vd.SetAlert(err)
+		u.LoginView.Render(w, vd)
+		return
+	}
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
+}
+
+// signIn is used to sign the given user in via cookies
+func (u *Users) signIn(w http.ResponseWriter, user *models.User) error {
+	if user.Remember == "" {
+		token, err := rand.RememberToken()
+		if err != nil {
+			return err
+		}
+		user.Remember = token
+		err = u.us.Update(user)
+		if err != nil {
+			return err
+		}
+	}
+
+	cookie := http.Cookie{
+		Name:     "remember_token",
+		Value:    user.Remember,
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &cookie)
+	return nil
+}
+
+// CookieTest is used to display cookies set on the current user
+func (u *Users) CookieTest(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("remember_token")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	user, err := u.us.ByRemember(cookie.Value)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, user)
+}
+*/
--- main.go
+++ main.go
@@ -23,11 +23,12 @@ func main() {
 	services, err := models.NewServices(psqlInfo)
 	must(err)
 	defer services.Close()
-	services.DestructiveReset() // delete the table and reset
-	//services.AutoMigrate()
+	//services.DestructiveReset() // delete the table and reset
+	services.AutoMigrate()
 
 	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers(services.User)
+	galleriesC := controllers.NewGalleries(services.Gallery)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
@@ -37,6 +38,10 @@ func main() {
 	r.Handle("/login", usersC.LoginView).Methods("GET")
 	r.HandleFunc("/login", usersC.Login).Methods("POST")
 	r.HandleFunc("/cookietest", usersC.CookieTest).Methods("GET")
+
+	// Gallery routes
+	r.Handle("/galleries/new", galleriesC.New).Methods("GET")
+
 	http.ListenAndServe(":3000", r)
 }
 
